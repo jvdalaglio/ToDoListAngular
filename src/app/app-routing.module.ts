1df150d6d7766947033d72a1b076be5594dae057
jvdalaglio <jvdalaglio@gmail.com>
2023-05-08 19:49:16 -0300

    creating and configure routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagesModule } from './pages/pages.module';
 
 const routes: Routes = [
-
+  {
+    path: '',
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
+  }
 ];
 
 @NgModule({
