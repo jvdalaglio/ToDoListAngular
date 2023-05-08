f6fcd64250e7972bb51e9ff424de1e7922091bd2
jvdalaglio <jvdalaglio@gmail.com>
2023-05-08 20:16:54 -0300

    getting time

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,13 +12,27 @@ export class HomeComponent implements OnInit {
     private fakeApiService: FakeApiService
   ) {}
 
-  users: any;
+  public users: any;
+  public hours: number = new Date().getHours();
+  public timeMessage: any;
 
   ngOnInit(): void {
     this.getUsers()
+    this.getTimeMessage()
   }
 
   getUsers() {
     this.fakeApiService.getUsers().subscribe(res => this.users = res)
   }
+
+  getTimeMessage() {
+    if(this.hours > 17 && this.hours < 24 || this.hours >= 0 && this.hours < 5) {
+      this.timeMessage = "Boa noite"
+    } else if(this.hours > 5 && this.hours < 12) {
+      this.timeMessage = "Bom dia"
+    } else {
+      this.timeMessage = "Boa tarde"
+    }
+  }
+
 }
